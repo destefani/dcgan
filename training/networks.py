29c1f2d772cc0286fe5244e5d63c1566fc0de35c
Marcelo Destefani <marcelo@hastapronto.cl>
2022-04-15 11:22:05 +0200

    networks

---
+++ training/networks.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    def __init__(self,
+        z_dim,        # Input latent (Z) dimensionality
+        channel_dim,                    
+        num_features,
+        ngpu
+    ):
+        super().__init__()
+        self.z_dim = z_dim
+        self.channel_dim = channel_dim
+        self.num_features = num_features
+        self.ngpu = ngpu
+
+        self.main = nn.Sequential(
+            # input is Z, going into a convolution
+            nn.ConvTranspose2d(z_dim, num_features * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(num_features * 8),
+            nn.ReLU(True),
+            # state size. (ngf*8) x 4 x 4
+            nn.ConvTranspose2d(num_features * 8, num_features * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features * 4),
+            nn.ReLU(True),
+            # state size. (ngf*4) x 8 x 8
+            nn.ConvTranspose2d(num_features * 4, num_features * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features * 2),
+            nn.ReLU(True),
+            # state size. (ngf*2) x 16 x 16
+            nn.ConvTranspose2d(num_features * 2, num_features, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d(num_features, channel_dim, 4, 2, 1, bias=False),
+            nn.Tanh()
+            # state size. (nc) x 64 x 64
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self,
+        channel_dim,
+        num_features,
+        ngpu
+    ):
+        super().__init__()
+        self.channel_dim = channel_dim
+        self.num_features = num_features
+        self.ngpu = ngpu
+
+        self.main = nn.Sequential(
+            # input is (nc) x 64 x 64
+            nn.Conv2d(channel_dim, num_features, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            nn.Conv2d(num_features, num_features * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16
+            nn.Conv2d(num_features * 2, num_features * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            nn.Conv2d(num_features * 4, num_features * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(num_features * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+            nn.Conv2d(num_features * 8, 1, 4, 1, 0, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
